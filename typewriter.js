e363b24b39bb06c4a8bbb08287c31a339625c27c
Soseh <sosehv@gmail.com>
2024-12-15 13:13:35 -0500

    Implemented typewriter async code

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let delayValue = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char + " ")
+  }, delayValue) // <= 1s delay to make it noticeable. Can dial it down later.
+  delayValue += 50;
+};
+process.stdout.write("\n");
